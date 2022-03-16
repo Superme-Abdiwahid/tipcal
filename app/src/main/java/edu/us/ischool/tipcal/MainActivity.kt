d08769c53a8e32f92d3d94cca397e9d6c436124e
Abdiwahid <ahajir@uw.edu>
2022-03-16 16:00:11 -0700

    update to tip calculator

--- app/src/main/java/edu/us/ischool/tipcal/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/tipcal/MainActivity.kt
@@ -1,59 +1,78 @@
 package edu.us.ischool.tipcal
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doOnTextChanged
-import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     private var total = 0.0;
     private var count = 0.0
-    private var overall = 0.0
+    private var overall = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-        var text: TextView = findViewById<TextView>(R.id.text_id)
-
-        var editText: EditText = findViewById<EditText>(R.id.editTextNumber)
-
         var getButton: Button = findViewById<Button>(R.id.btn_button)
         getButton.isEnabled = false;
 
-        editText.addTextChangedListener{
-            getButton.isEnabled = true;
-            var input = editText.text.toString().toDouble();
-
-            Log.v("MAIN_ACTIVITY", input.toString())
-            total = input;
-            count = total * 0.15;
-
-
-
-
-        }
-        val format: NumberFormat = NumberFormat.getCurrencyInstance(Locale.US)
-        getButton.doOnTextChanged { text, start, before, count ->
-            getButton.text = format.format(overall)
-        }
-        getButton.setOnClickListener{
-            val message = Toast.makeText(this, "Thanks for your tip your tip was $" + "$overall", Toast.LENGTH_LONG)
+        var editText: EditText = findViewById<EditText>(R.id.editTextNumber)
+        try {
+            editText.addTextChangedListener(object : TextWatcher {
+                private var current = ""
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+                override fun afterTextChanged(s: Editable) {
+                    if (s.toString() != current) {
+                        editText.removeTextChangedListener(this)
+                        val currency = String.format("[%s,.\\s]", "$")
+                        val cleanString = s.toString().replace(currency.toRegex(), "")
+                        var inputNum = 0.0;
+                        if (!cleanString.equals("")) {
+                            inputNum = cleanString.toDouble()
+                            System.clearProperty(this.toString())
+                            getButton.isEnabled = true;
+                            total = inputNum;
+                            count = total * 0.15;
+                    //        System.arraycopy(arrayOf(this), 1, cleanString.split("/s"), 1, 1)
+                           // System.arraycopy(0,0, cleanString.split("/s"), 1, currency.toString().length)
+                            overall = NumberFormat.getCurrencyInstance().format(count / 100)
+                        } else {
+                            getButton.isEnabled = false;
+                        }
+                        val formatted = NumberFormat.getCurrencyInstance().format(inputNum / 100)
+                        current = formatted
+                        editText.setText(formatted)
+                        editText.setSelection(formatted.length)
+                        editText.addTextChangedListener(this)
+                    }
+                }
+            })
+            getButton.setOnClickListener {
+                val message = Toast.makeText(
+                    this,
+                    "Thanks for your tip your tip was $overall",
+                    Toast.LENGTH_LONG
+                )
+                message.show();
+            }
+        }catch(e: Exception){
+            val message = Toast.makeText(
+                this,
+                "An erorr of" + e.stackTrace + "Has occured",
+                Toast.LENGTH_LONG
+            )
             message.show();
         }
-
-
-
-
     }
 }
